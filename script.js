d8a52e8a400c3e7c4174611a154699c3871995d2
Reda1000 <david@fam-renner.de>
2021-08-15 13:53:46 +0200

    3 files modified

--- script.js
+++ script.js
@@ -1,132 +1,42 @@
 console.log('hello!');
 
 var chartDom = document.getElementById('chart1');
-setTimeout(() => {
 var myChart = echarts.init(chartDom);
 var option;
 
 option = {
   title: {
-    text: '天气情况统计',
-    subtext: '虚构数据',
-    left: 'center'
-  },
-  tooltip: {
-    trigger: 'item',
-    formatter: '{a} <br/>{b} : {c} ({d}%)'
+    show: false,
+    text: 'Gefühle',
+    subtext: 'In Deutschland von 2019'
   },
   legend: {
     bottom: 10,
     left: 'center',
-    data: ['西凉', '益州', '兖州', '荆州', '幽州']
+    data: ['Glück', 'Wut', 'Trauer', 'Freude'],
+    textStyle: {
+      fontSize: '15',
+    },
   },
   series: [
     {
       type: 'pie',
-      radius: '65%',
-      center: ['50%', '50%'],
-      selectedMode: 'single',
+      radius: '85%',
+      center: ['50%', '45%'],
+      label: {
+        show: false
+      },
       data: [
-        {
-          value: 1548,
-          name: '幽州',
-          label: {
-            formatter: [
-              '{title|{b}}{abg|}',
-              '  {weatherHead|天气}{valueHead|天数}{rateHead|占比}',
-              '{hr|}',
-              '  {Sunny|}{value|202}{rate|55.3%}',
-              '  {Cloudy|}{value|142}{rate|38.9%}',
-              '  {Showers|}{value|21}{rate|5.8%}'
-            ].join('\n'),
-            backgroundColor: '#eee',
-            borderColor: '#777',
-            borderWidth: 1,
-            borderRadius: 4,
-            rich: {
-              title: {
-                color: '#eee',
-                align: 'center'
-              },
-              abg: {
-                backgroundColor: '#333',
-                width: '100%',
-                align: 'right',
-                height: 25,
-                borderRadius: [4, 4, 0, 0]
-              },
-              Sunny: {
-                height: 30,
-                align: 'left',
-                backgroundColor: {
-                  image: weatherIcons.Sunny
-                }
-              },
-              Cloudy: {
-                height: 30,
-                align: 'left',
-                backgroundColor: {
-                  image: weatherIcons.Cloudy
-                }
-              },
-              Showers: {
-                height: 30,
-                align: 'left',
-                backgroundColor: {
-                  image: weatherIcons.Showers
-                }
-              },
-              weatherHead: {
-                color: '#333',
-                height: 24,
-                align: 'left'
-              },
-              hr: {
-                borderColor: '#777',
-                width: '100%',
-                borderWidth: 0.5,
-                height: 0
-              },
-              value: {
-                width: 20,
-                padding: [0, 20, 0, 30],
-                align: 'left'
-              },
-              valueHead: {
-                color: '#333',
-                width: 20,
-                padding: [0, 20, 0, 30],
-                align: 'center'
-              },
-              rate: {
-                width: 40,
-                align: 'right',
-                padding: [0, 10, 0, 0]
-              },
-              rateHead: {
-                color: '#333',
-                width: 40,
-                align: 'center',
-                padding: [0, 10, 0, 0]
-              }
-            }
-          }
-        },
-        { value: 735, name: '荆州' },
-        { value: 510, name: '兖州' },
-        { value: 434, name: '益州' },
-        { value: 335, name: '西凉' }
-      ],
-      emphasis: {
-        itemStyle: {
-          shadowBlur: 10,
-          shadowOffsetX: 0,
-          shadowColor: 'rgba(0, 0, 0, 0.5)'
-        }
-      }
+        { value: 1548, name: 'Glück' },
+        { value: 735, name: 'Wut' },
+        { value: 510, name: 'Trauer' },
+        { value: 434, name: 'Freude' }
+      ]
     }
   ]
 };
 
 option && myChart.setOption(option);
-}, 1000);
+window.onresize = function() {
+  myChart.resize();
+};
